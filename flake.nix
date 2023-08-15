1b1aa40ea11671c1d91eb6068aee3748df757dd8
Mika Tammi <mika.tammi@unikie.com>
2023-08-15 11:40:46 +0300

    Initial flake for running Robot Framework tests
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "A flake for for running Robot Framework tests";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: let
+    systems = with flake-utils.lib.system; [
+      x86_64-linux
+      aarch64-linux
+    ];
+  in
+    flake-utils.lib.eachSystem systems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.ghaf-robot = pkgs.callPackage ./Robot-Framework {};
+      packages.default = self.packages.${system}.ghaf-robot;
+
+      # Development shell
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (python3.withPackages (ps:
+            with ps; [
+              robotframework
+              robotframework-sshlibrary
+              pyserial
+            ]))
+        ];
+      };
+
+      # Allows formatting files with `nix fmt`
+      formatter = pkgs.alejandra;
+    });
+}
