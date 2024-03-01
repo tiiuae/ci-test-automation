01ba8eaf7905908305e567c354eddd10ca3a4680
Mariia Azbeleva <mariia.azbeleva@unikie.com>
2024-03-01 15:40:49 +0200

    Add FileIO test
    Signed-off-by: Mariia Azbeleva <mariia.azbeleva@unikie.com>

--- Robot-Framework/lib/PerformanceDataProcessing.py
+++ Robot-Framework/lib/PerformanceDataProcessing.py
@@ -68,6 +68,18 @@ class PerformanceDataProcessing:
                 self.device]
         self._write_to_csv(test_name, data)
 
+    def write_fileio_data_to_csv(self, test_name, data):
+        data = [self.build_number,
+                data['file_operations'],
+                data['throughput'],
+                data['min_latency'],
+                data['avg_latency'],
+                data['max_latency'],
+                data['avg_events_per_thread'],
+                data['events_per_thread_stddev'],
+                self.device]
+        self._write_to_csv(test_name, data)
+
     @keyword
     def read_cpu_csv_and_plot(self, test_name):
         build_numbers = []
@@ -256,19 +268,97 @@ class PerformanceDataProcessing:
         plt.savefig(f'../test-suites/{self.device}_{test_name}.png')  # Save the plot as an image file
 
     @keyword
+    def read_fileio_data_csv_and_plot(self, test_name):
+        build_numbers = []
+        file_operations = []
+        throughput = []
+        min_latency = []
+        avg_latency = []
+        max_latency = []
+        avg_events_per_thread = []
+        events_per_thread_stddev = []
+
+        with open(f"{self.data_dir}{self.device}_{test_name}.csv", 'r') as csvfile:
+            csvreader = csv.reader(csvfile)
+            logging.info("Reading data from csv file...")
+            for row in csvreader:
+                if row[8] == self.device:
+                    build_numbers.append(str(row[0]))
+                    file_operations.append(float(row[1]))
+                    throughput.append(float(row[2]))
+                    min_latency.append(float(row[3]))
+                    avg_latency.append(float(row[4]))
+                    max_latency.append(float(row[5]))
+                    avg_events_per_thread.append(float(row[6]))
+                    events_per_thread_stddev.append(float(row[7]))
+
+        print(file_operations)
+        print(throughput)
+        print(min_latency)
+        print(events_per_thread_stddev)
+
+        plt.figure(figsize=(20, 10))
+        plt.set_loglevel('WARNING')
+
+        plt.subplot(3, 1, 1)
+        plt.ticklabel_format(axis='y', style='plain')
+        plt.plot(build_numbers, file_operations, marker='o', linestyle='-', color='b')
+        plt.yticks(fontsize=14)
+        plt.title('File operation', loc='right', fontweight="bold", fontsize=16)
+        plt.ylabel('File operation per second', fontsize=16)
+        plt.grid(True)
+        plt.xticks(build_numbers, fontsize=14)
+
+        plt.subplot(3, 1, 2)
+        plt.ticklabel_format(axis='y', style='plain')
+        plt.plot(build_numbers, throughput, marker='o', linestyle='-', color='b')
+        plt.yticks(fontsize=14)
+        plt.title('Throughput', loc='right', fontweight="bold", fontsize=16)
+        plt.ylabel('Throughput, MiB/s', fontsize=16)
+        plt.grid(True)
+        plt.xticks(build_numbers, fontsize=14)
+
+        plt.subplot(3, 1, 3)
+        plt.ticklabel_format(axis='y', style='plain')
+        plt.plot(build_numbers, avg_latency, marker='o', linestyle='-', color='b', label='Avg')
+        plt.ylabel('Avg Latency (ms)', fontsize=16)
+        plt.legend(loc='upper left')
+        plt.xlabel('Build Number', fontsize=16)
+        plt.twinx()
+        plt.plot(build_numbers, max_latency, marker='o', linestyle='-', color='r', label='Max')
+        plt.plot(build_numbers, min_latency, marker='o', linestyle='-', color='g', label='Min')
+        plt.yticks(fontsize=14)
+        plt.ylabel('Max/Min Latency (ms)', fontsize=16)
+        plt.legend(loc='upper right')
+        plt.title('Latency', loc='right', fontweight="bold", fontsize=16)
+        plt.grid(True)
+        plt.xticks(build_numbers, fontsize=14)
+
+        plt.suptitle(f'{test_name} ({self.device})', fontsize=18, fontweight='bold')
+
+        plt.tight_layout()
+        plt.savefig(f'../test-suites/{self.device}_{test_name}.png')  # Save the plot as an image file
+
+    @keyword
     def save_cpu_data(self, test_name, cpu_data):
 
         self.write_cpu_to_csv(test_name, cpu_data)
         self.read_cpu_csv_and_plot(test_name)
 
     @keyword
-    def save_memory_data(self, test_name, cpu_data):
+    def save_memory_data(self, test_name, memory_data):
 
-        self.write_mem_to_csv(test_name, cpu_data)
+        self.write_mem_to_csv(test_name, memory_data)
         self.read_mem_csv_and_plot(test_name)
 
     @keyword
-    def save_speed_data(self, test_name, cpu_data):
+    def save_speed_data(self, test_name, speed_data):
 
-        self.write_speed_to_csv(test_name, cpu_data)
+        self.write_speed_to_csv(test_name, speed_data)
         self.read_speed_csv_and_plot(test_name)
+
+    @keyword
+    def save_fileio_data(self, test_name, fileio_data):
+
+        self.write_fileio_data_to_csv(test_name, fileio_data)
+        self.read_fileio_data_csv_and_plot(test_name)
