36e152c8f58509ca5641f539fc814083dc1361b6
Milla Valio <milla.valio@unikie.com>
2026-02-18 14:58:31 +0200

    Update to Nixos 25.11 - second try
    Signed-off-by: Milla Valio <milla.valio@unikie.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A flake for for running Robot Framework tests";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -74,6 +74,7 @@
                 opencv4
                 paramiko
                 evdev
+                matplotlib
               ])
               ++ (with self.packages.${system}; [
                 robotframework-jsonlibrary
